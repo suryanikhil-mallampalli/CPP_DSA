126afbad6883b1d3cb0f93b79aedcd5ec395f278
Surya Nikhil Mallampalli <suryanikhil.m@gmail.com>
2022-07-11 21:05:42 +0530

    Add files via upload

---
+++ ap17.cpp
@@ -0,0 +1,21 @@
+//for numbers pyramid
+//  1
+// 1 2 
+//1 2 3
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+     for(int i=1;i<=n;i++){
+        for(int j=1;j<=n-i;j++){
+            cout<<" ";
+        }
+        for(int k=1;k<=i;k++){
+            cout<<k;
+            cout<<" ";
+        }
+        cout<<endl;
+     }
+}
