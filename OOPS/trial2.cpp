dc326c81ee5360794c5152bebc4401133bac7dec
suryanikhil-mallampalli <suryanikhil_mallampalli@srmap.edu.in>
2023-02-23 23:12:13 +0530

    Create trial2.cpp

---
+++ OOPS/trial2.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+class B;
+  
+class A 
+{ 
+    public: 
+        void showB(B &x); 
+}; 
+  
+class B
+{ 
+    private: 
+        int b; 
+    public: 
+        B() : b(0) {}
+        
+        friend void A::showB(B &x); 
+}; 
+ 
+void A::showB(B &x) 
+{
+    cout << "B::b = " << x.b;
+} 
+  
+int main() 
+{ 
+    A a; 
+    B x;
+    
+    a.showB(x);
+    
+    return 0; 
+}
