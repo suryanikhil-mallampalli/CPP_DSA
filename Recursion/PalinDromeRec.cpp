ae6ff90b7ea7eeee990b59aa59fcbdcf36593cce
suryanikhil-mallampalli <suryanikhil_mallampalli@srmap.edu.in>
2023-03-14 23:23:35 +0530

    update

--- Recursion/PalinDromeRec.cpp
+++ Recursion/PalinDromeRec.cpp
@@ -1,8 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+bool isPalindrome(string str, int start, int end)
+{
+	if(start >= end)
+		return true;
+
+	return ((str[start]==str[end]) && 
+		     isPalindrome(str, start + 1, end - 1));
+}
 //Main function
 int main(){
-    
+    string s = "Swagagaws";
+	
+		printf("%s", isPalindrome(s, 0, s.length() -1) ? "true" : "false");
+	
     return 0;
 }
