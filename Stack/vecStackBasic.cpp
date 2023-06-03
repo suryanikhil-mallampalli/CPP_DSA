b2ea6e1b028d20c9342befb3dc69ceb08f2f3c7b
suryanikhil-mallampalli <suryanikhil_mallampalli@srmap.edu.in>
2023-06-03 10:52:06 +0530

    stacks Basics

---
+++ Stack/vecStackBasic.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.H>
+using namespace std;
+struct Mystack{
+    vector<int> v;
+    void push(int x){
+        v.push_back(x);
+    }
+    int pop(){
+        int res=v.back();
+        v.pop_back();
+        return res;
+    }
+    int peek(){
+        return v.back();
+    }
+    int size(){
+        return v.size();
+    }
+    bool isEmpty(){
+        return v.empty();
+    }
+};
+int main(){
+    Mystack s;
+    s.push(5);
+    s.push(10);
+    s.push(15);
+    cout<<s.pop()<<endl;
+    cout<<s.size()<<endl;
+    cout<<s.peek()<<endl;
+    cout<<s.isEmpty()<<endl;
+    return 0;
+}
+// this will be a dynamical stack.
